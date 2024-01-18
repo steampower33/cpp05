25522b6142fc953bc032b40472a2fc07f577a580
SeungMinLee <seunlee2@student.42seoul.kr>
2024-01-18 22:47:44 +0900

    init ex00

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,11 @@
+#include "Bureaucrat.hpp"
+
+class Bureaucrat {
+public:
+	Bureaucrat();
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat& operator=(const Bureaucrat& other);
+	~Bureaucrat();
+private:
+
+};
