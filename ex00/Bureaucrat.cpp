a2b6091c31ac5b821a11d012faf40635671a9ba4
SeungMinLee <seunlee2@student.42seoul.kr>
2024-01-20 17:24:40 +0900

    ex00

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -1,11 +1,60 @@
 #include "Bureaucrat.hpp"
 
-class Bureaucrat {
-public:
-	Bureaucrat();
-	Bureaucrat(const Bureaucrat& other);
-	Bureaucrat& operator=(const Bureaucrat& other);
-	~Bureaucrat();
-private:
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name), _grade(grade) {
+	if (_grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (_grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
 
-};
+Bureaucrat::Bureaucrat(const Bureaucrat& other) {
+	*this = other;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
+	if (this != &other)
+	{
+		_grade = other._grade;
+	}
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat() {
+	std::cout << _name << " destructor" << std::endl;
+}
+
+std::string Bureaucrat::getName() const {
+	return _name;
+}
+
+int Bureaucrat::getGrade() const {
+	return _grade;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+	return "Grade Too High";
+}
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+	return "Grade Too Low";
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b) {
+	os << b.getName() << ", bureaucrat grade " << b.getGrade() << ".";
+	return os;
+}
+
+void Bureaucrat::incrementGrade() {
+	_grade--;
+	if (_grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (_grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+void Bureaucrat::decrementGrade() {
+	_grade++;
+	if (_grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (_grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,1 +1,45 @@
 #include "Bureaucrat.hpp"
+
+int main() {
+	try
+	{
+		try
+		{
+			Bureaucrat a("a", 151);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		Bureaucrat a("a", 0);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	Bureaucrat a("a", 1);
+	Bureaucrat b("b", 150);
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	
+	try
+	{
+		a.incrementGrade();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		b.decrementGrade();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	return 0;
+}
