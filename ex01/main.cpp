8c78bd3c5bfdae7bb3f9ff86cfdee3e8c5b9e394
SeungMinLee <seunlee2@student.42seoul.kr>
2024-01-22 18:17:51 +0900

    ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,19 +3,23 @@
 
 int main() {
 	Bureaucrat b("LEE", 10);
-	Form f1("42Seoul Subsidy Policy", 15, 15);
-	Form f2("Survive Policy", 5, 5);
+	std::cout << b << std::endl;
 
+	std::cout << std::endl;
+
+	Form f1("42Seoul Subsidy Policy", 15, 15);
+	std::cout << f1 << std::endl;
 	f1.beSigned(b);
 	b.signForm(f1);
-	std::cout << b << std::endl;
 	std::cout << f1 << std::endl;
 
 	std::cout << std::endl;
 
+	Form f2("Survive Policy", 5, 5);
+	std::cout << f2 << std::endl;
 	f2.beSigned(b);
 	b.signForm(f2);
-	std::cout << b << std::endl;
 	std::cout << f2 << std::endl;
+
 	return 0;
 }
